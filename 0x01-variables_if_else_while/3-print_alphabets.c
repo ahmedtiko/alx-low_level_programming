dccaa22155912163c55698c0fdbf0b47f851a26d
ahmedtiko <ahmedmmtarek@gmail.com>
2023-10-07 09:55:21 -0700

    printing alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,26 +5,21 @@
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	char letter = 'a';
-
 	while (letter <= 'z')
-       	{
-		putchar(letter);
-		letter++;
-	}
-
-	letter = 'A';
-
-	while (letter <= 'Z') 
 	{
 		putchar(letter);
 		letter++;
 	}
 
+	letter = 'A';
+	while (letter <= 'Z') 
+	{
+		putchar(letter);
+		letter++;
+	}
 	putchar('\n');
 	return (0);
 }
-
