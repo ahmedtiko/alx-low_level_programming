97e9573c85d9add52cda7c69ceb641395448cfa2
ahmedtiko <ahmedmmtarek@gmail.com>
2023-10-10 19:07:11 -0700

    tasks 5 to 8

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,7 +5,7 @@
  *    * Return: 1 and prints + if n is greater than zero
  *     * 0 and prints 0 if n is zero
  *      * -1 and prints - if n is less than zero
- *       */
+ */
 int print_sign(int n)
 {
 	if (n > 0)
@@ -13,12 +13,12 @@ int print_sign(int n)
 		_putchar('+');
 		return (1);
 	}
-       	else if (n == 0)
+	else if (n == 0)
 	{
 		_putchar(48);
 		return (0);
 	}
-       	else if (n < 0)
+	else if (n < 0)
 	{
 		_putchar('-');
 	}
