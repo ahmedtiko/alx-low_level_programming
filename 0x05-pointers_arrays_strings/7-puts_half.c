bc708983f6dec43ba152ba9a44024e3164cf1ca0
ahmedtiko <ahmedmmtarek@gmail.com>
2023-10-16 16:17:28 -0700

    AutoPush

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * puts_half - a function that prints half of a string
+ * if odd len, n = (length_of_the_string - 1) / 2
+ * @str: input
+ * Return: half of input
+ */
+
+void puts_half(char *str)
+{
+	int length = 0;
+	int i;
+
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+
+	int start = length / 2;
+	int end = length;
+
+	if (length % 2 == 1)
+	{
+		end = start + 1;
+		start = (length - 1) / 2;
+	}
+
+	for (i = start; i < end; i++)
+	{
+		_putchar(str[i])
+	}
+
+	_putchar('\n');
+}	
