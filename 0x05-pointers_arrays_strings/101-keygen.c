edb1c04aa3c6c69b89a3c0c19b80381d55c588e2
ahmedtiko <ahmedmmtarek@gmail.com>
2023-10-16 17:33:38 -0700

    AutoPush

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * random_char - Returns a random character from the given charset.
+ * @charset: The character set to choose from.
+ * @charset_length: The length of the character set.
+ * Return: A random character.
+ */
+
+
+char random_char(const char *charset, int charset_length) 
+{
+    int index = rand() % charset_length;
+    return charset[index];
+}
+
+/**
+ * main - Entry point for the program.
+ * Return: Always 0.
+ */
+
+int main() 
+{
+    const char uppercase_letters[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const char digits[] = "0123456789";
+    int num_uppercase = 7;  // Example: 7 uppercase letters
+    int num_digits = 3;     // Example: 3 digits
+    int total_length = 10;  // Fixed length required by 101-crackme
+
+    srand(time(NULL));
+
+    char password[11];  // One extra character for the null terminator
+
+    if (total_length != num_uppercase + num_digits) {
+        printf("Invalid password length.\n");
+        return 1;
+    }
+
+    for (int i = 0; i < num_uppercase; i++) {
+        password[i] = random_char(uppercase_letters, sizeof(uppercase_letters) - 1);
+    }
+
+    for (int i = num_uppercase; i < total_length; i++) {
+        password[i] = random_char(digits, sizeof(digits) - 1);
+    }
+
+    password[total_length] = '\0';
+
+    // Shuffle the password to make it random
+    for (int i = 0; i < total_length - 1; i++) {
+        int j = i + rand() % (total_length - i);
+        char temp = password[i];
+        password[i] = password[j];
+        password[j] = temp;
+    }
+
+    printf("Generated Password: %s\n", password);
+
+    return (0);
+}
