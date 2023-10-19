62e9828c942e119f6a508092f38ca57633a29209
ahmedtiko <ahmedmmtarek@gmail.com>
2023-10-19 05:25:54 -0700

    AutoPush

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,18 +11,15 @@
 char *cap_string(char *str)
 {
 	int i = 0;
-	int capitalize = 1;
 
-	while (str[i] != '\0')
+	while (str[i])
 	{
-		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || str[i] == ',' || str[i] == ';' || str[i] == '.' || str[i] == '!' || str[i] == '?' || str[i] == '"' || str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}')
+		if (str[i] >= 'a' && str[i] <= 'z')
 		{
-			capitalize = 1;
-		}
-		else if (capitalize && (str[i] >= 'a' && str[i] <= 'z'))
-		{
-			str[i] = str[i] - 32;
-			capitalize = 0;
+			if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || str[i] == ',' || str[i] == ';' || str[i] == '.' || str[i] == '!' || str[i] == '?' || str[i] == '"' || str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}')
+			{
+				str[i] = str[i] - 32;
+			}
 		}
 
 		i++;
