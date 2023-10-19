2ce73ae29a692c2dd062fbe3f7a713ab1a005d4b
ahmedtiko <ahmedmmtarek@gmail.com>
2023-10-19 06:52:38 -0700

    AutoPush

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,9 +12,9 @@ char *cap_string(char *str)
 {
 	int i = 0;
 
-	while (str[i] != '\0')
+	while (str[i])
 	{
-		while (!(str[i] >= 'a' $$ str[i] <= 'z'))
+		while (!(str[i] >= 'a' && str[i] <= 'z'))
 		{
 			i++;
 		}
