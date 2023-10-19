fb9a56b77505cb1a5bdb70a42f6a237c8b62ff60
ahmedtiko <ahmedmmtarek@gmail.com>
2023-10-19 04:55:07 -0700

    AutoPush

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,21 +8,20 @@
   * Return: Void.
   */
 
-char *cap_string(char *)
+char *cap_string(char *str)
 {
 	int i = 0;
-	int capitalize = 1;
 
-	while (str[i] != '\0')
+	while (str[i])
 	{
-		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || str[i] == ',' || str[i] == ';' || str[i] == '.' || str[i] == '!' || str[i] == '?' || str[i] == '"' || str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}')
+		while (!(str[i] >= 'a' && str[i] <= 'z'))
 		{
-			capitalize = 1;
+			i++;
 		}
-		else if (capitalize && (str[i] >= 'a' && str[i] <= 'z'))
+
+		if (str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' || str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' || str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"' || str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' || str[i - 1] == '}' || i = 0)
 		{
-			str[i] = str[i] - 32;
-			capitalize = 0;
+			str[i] -= 32;
 		}
 
 		i++;
