3eecdae90e0f516ca0122f6090807ea87f2f18fe
ahmedtiko <ahmedmmtarek@gmail.com>
2023-10-25 10:58:27 -0700

    AutoPush

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * is_divisible - checks if a number is divisible by another number
+ * @n: the number to be checked
+ * @divisor: the number to divide by
+ * Return: 1 if divisible, 0 if not
+ */
+
+int is_divisible(int n, int div)
+{
+	if (div <= 1)
+	{
+		return (0);
+	}
+	else if (div == 2)
+	{
+		return ((n % div == 0) ? 1 : 0);
+	}
+	else if (n % div == 0)
+	{
+		return (1);
+	}
+
+	return is_divisible(n, div - 1);
+}
+
+/**
+ * is_prime_number - checks if a number is a prime number
+ * @n: the number to be checked
+ * Return: 1 if prime, 0 if not
+ */
+
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+	else if (n <= 3)
+	{
+		return (1);
+	}
+	else if (is_divisible(n, n - 1))
+	{
+		return (0);
+	}
+
+	return (1);
+}
