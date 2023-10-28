a2054931e72a5461f28a8c29e70306e6a719b5d2
ahmedtiko <ahmedmmtarek@gmail.com>
2023-10-27 18:20:05 -0700

    AutoPush

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,32 +1,6 @@
 #include "main.h"
 
 /**
- * is_positive_number - Check if a string is a positive number.
- * @str: The string to check.
- *
- * Return: 1 if str is a positive number, 0 otherwise.
- */
-
-int is_positive_number(char *str)
-{
-	int i;
-
-	i = 0;
-
-	while (str[i])
-	{
-		if (!isdigit(str[i]))
-		{
-			return (0);
-		}
-
-		i++;
-	}
-
-	return (1);
-}
-
-/**
  * main - Entry point of the program
  * @argc: The number of command-line arguments
  * @argv: An array containing the command-line arguments
@@ -35,25 +9,22 @@ int is_positive_number(char *str)
 
 int main(int argc, char *argv[])
 {
-	if (argc < 2)
-	{
-		printf("0\n");
-		return (0);
-	}
-
 	int sum = 0;
+	char *c;
 
-	for (i = 1; i < argc; i++)
+	while (--argc)
 	{
-		if (!is_positive_number(argv[i]))
+		for (c = argv[argc]; *c; c++)
 		{
-			printf("Error\n");
-			return (1);
+			if (*c < '0' || *c > '9')
+			{
+				return (printf("Error\n"), 1);
+			}
 		}
 
-		sum += atoi(argv[i]);
+		sum += atoi(argv[argc]);
 	}
 
 	printf("%d\n", sum);
 	return (0);
-}
+}	
