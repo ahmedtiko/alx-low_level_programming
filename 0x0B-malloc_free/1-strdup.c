a7d4d762eff21843fc2c885a79ed43723cba1d94
ahmedtiko <ahmedmmtarek@gmail.com>
2023-10-31 11:54:16 -0700

    AutoPush

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory
+ * containing a copy of the string
+ * @str: the string to duplicate
+ *
+ * Return: a pointer to the duplicated string, or NULL if it fails
+ */
+
+char *_strdup(char *str)
+{
+	char *duplicate;
+	unsigned int l = 0;
+	unsigned int i = 0;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	while (str[l])
+	{
+		length ++;
+	}
+
+	duplicate = (char *)malloc((l + 1) * sizeof(char));
+
+	if (duplicate == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < l; i++)
+	{
+		duplicate[i] = str[i];
+	}
+
+	duplicate[l] = '\0';
+
+	return (duplicate);
+}
