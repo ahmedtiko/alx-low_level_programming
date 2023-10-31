963c733abc1cb24d90576f21a59f61111b3fb283
ahmedtiko <ahmedmmtarek@gmail.com>
2023-10-31 12:57:21 -0700

    AutoPush

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * alloc_grid - Create a 2D array of integers
+ * @width: The width of the grid
+ * @height: The height of the grid
+ *
+ * Return: A pointer to the newly allocated 2D array.
+ * 	NULL if width or height is 0 or if memory allocation fails.
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+
+	int **grid = (int **)malloc(sizeof(int *) * height);
+
+	if (grid == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = (int *)malloc(sizeof(int) * width);
+		if (grid[i] == NULL)
+		{
+			for (j = 0; j < width; j++)
+			{
+				free(grid[j]);
+			}
+			free(grid);
+			return (NULL);
+		}
+		for (j = 0; j < width; j++)
+		{
+			grid[i][j] = 0;
+		}
+	}
+	return (grid);
+}
