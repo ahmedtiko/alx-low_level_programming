848c496ca70acace17008d5aacda71524fcaf687
ahmedtiko <ahmedmmtarek@gmail.com>
2023-10-31 14:15:21 -0700

    AutoPush

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * free_grid - Free a 2D grid previously created by alloc_grid
+ * @grid: The 2D grid to be freed
+ * @height: The height of the grid
+ *
+ * Return: No return value
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	if (grid != NULL)
+	{
+		for (i = 0; i < height; i++)
+		{
+			free(grid[i]);
+		}
+		free(grid);
+	}
+}
