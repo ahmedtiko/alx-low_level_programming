a1ae9a33cd50cfadc4e9ae7cf6b8a569e327a276
ahmedtiko <ahmedmmtarek@gmail.com>
2023-10-31 14:42:29 -0700

    AutoPush

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+ * main - Entry point for the program.
+ * @argc: The number of command-line arguments.
+ * @argv: An array of strings containing the command-line arguments.
+ *
+ * Return: 0 for success, 1 for error.
+ */
+
 int main(int argc, char *argv[])
 {
 	int n1, n2, r;
@@ -11,7 +19,9 @@ int main(int argc, char *argv[])
 
 	n1 = atoi(argv[1]);
 	n2 = atoi(argv[2]);
+
 	r = n1 * n2;
+
 	printf("%d\n", r);
 
 	return (0);
