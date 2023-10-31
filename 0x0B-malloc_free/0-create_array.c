691121241aaa527d45a38b6342d41361286f17e8
ahmedtiko <ahmedmmtarek@gmail.com>
2023-10-31 11:16:59 -0700

    AutoPush

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,25 +10,24 @@
 
 char *create_array(unsigned int size, char c)
 {
-	char *array;
+	char *array = NULL;
 	unsigned int i;
 
 	if (size == 0)
 	{
 		return (NULL);
 	}
-
-	array = (char *)malloc(size * sizeof(char));
-
-	if (array == NULL)
+	if (size != 0)
 	{
-		return (NULL);
-	}
+		array = (char *)malloc(size * sizeof(char));
 
-	for (i = 0; i < size; i++)
-	{
-		array[i] = c;
+		if (array != NULL)
+		{
+			for (i = 0; i < size; i++)
+			{
+				array[i] = c;
+			}
+		}
 	}
-
 	return (array);
 }
