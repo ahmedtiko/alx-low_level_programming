f46b5e0a06fdab69c4bbafb9fb610fcbfb2f8a3d
ahmedtiko <ahmedmmtarek@gmail.com>
2023-10-31 14:30:36 -0700

    AutoPush

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+ * main - The main function that prints the program name.
+ * @argc: The number of command-line arguments.
+ * @argv: An array of strings containing the command-line arguments.
+ *
+ * Return: Always 0 (success).
+ */
+
 int main(int argc, char *argv[])
 {
 	(void)argv;
