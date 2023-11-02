7aaabd088cecc47307934e65767bb9b7f2da0f8c
ahmedtiko <ahmedmmtarek@gmail.com>
2023-11-02 05:58:18 -0700

    AutoPush

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * malloc_checked - Allocate memory using malloc
+ * @b: The number of bytes to allocate
+ *
+ * Return: A pointer to the allocated memory
+ * or terminate with status value 98 if malloc fails
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+
+	if (ptr == NULL)
+	{
+		_putchar("Error: malloc failed\n");
+		exit(98);
+	}
+
+	return (ptr);
+}
