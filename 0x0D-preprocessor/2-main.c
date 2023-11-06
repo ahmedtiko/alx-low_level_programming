fee5468b47af0a6ede155b59d18b653db5537234
ahmedtiko <ahmedmmtarek@gmail.com>
2023-11-06 08:36:19 -0800

    AutoPush

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function prints the name of the file it was compiled from.
+ *
+ *Return: 0 on success
+ */
 int main(void)
 {
 	printf("%s\n", __FILE__);
