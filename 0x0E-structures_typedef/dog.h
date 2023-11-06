984c4f9c2f40b10e82581f0e14ab5220279a9fca
ahmedtiko <ahmedmmtarek@gmail.com>
2023-11-06 13:57:03 -0800

    AutoPush

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
-
+/**
+ * struct dog - Represents information about a dog.
+ *
+ * @name: Pointer to a character array for the dog's name.
+ * @age: The age of the dog as a floating-point number.
+ * @owner: Pointer to a character array for the owner's name.
+ */
 struct dog
 {
 	char *name;
