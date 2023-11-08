985c2d5d4ae1c609428b9eb3954e13939d650aed
ahmedtiko <ahmedmmtarek@gmail.com>
2023-11-08 13:25:33 -0800

    AutoPush

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,6 +1,5 @@
 #include "function_pointers.h"
-#include <stddef.h>
-#include <stdio.h>
+
 /**
  * array_iterator - Iterates over an array and applies a function to each element.
  * @array: Pointer to an integer array.
@@ -15,7 +14,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	if (array == NULL || size == NULL || action == NULL)
+	if (array == NULL || size == 0 || action == NULL)
 	{
 		return;
 	}
