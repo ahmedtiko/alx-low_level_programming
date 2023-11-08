89cce800c8a329eb08c919770b7389bb3f15d8b0
ahmedtiko <ahmedmmtarek@gmail.com>
2023-11-08 14:40:31 -0800

    AutoPush

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,53 +1,40 @@
-#include <stdio.h>
 #include "3-calc.h"
-
 /**
  * op_add - Adds two integers.
  * @a: First integer
  * @b: Second integer
- *
  * Return: Result of addition.
  */
-
 int op_add(int a, int b)
 {
 	return (a + b);
 }
-
 /**
  * op_sub - Subtracts two integers.
  * @a: First integer
  * @b: Second integer
- *
  * Return: Result of subtraction.
  */
-
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
-
 /**
  * op_mul - Multiplies two integers.
  * @a: First integer
  * @b: Second integer
- *
  * Return: Result of multiplication.
  */
-
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
-
 /**
  * op_div - Divides two integers.
  * @a: First integer
  * @b: Second integer
- *
  * Return: Result of division.
  */
-
 int op_div(int a, int b)
 {
 	if (b == 0)
@@ -55,18 +42,14 @@ int op_div(int a, int b)
 		printf("Error\n");
 		exit(100);
 	}
-
 	return (a / b);
 }
-
 /**
  * op_mod - Calculates the modulo of two integers.
  * @a: First integer
  * @b: Second integer
- *
  * Return: Result of modulo operation.
  */
-
 int op_mod(int a, int b)
 {
 	if (b == 0)
@@ -74,6 +57,5 @@ int op_mod(int a, int b)
 		printf("Error\n");
 		exit(100);
 	}
-
 	return (a % b);
 }
