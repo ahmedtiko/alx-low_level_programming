976821ac4ecf6929b74992cf16013005555e699c
ahmedtiko <ahmedmmtarek@gmail.com>
2023-11-08 12:41:21 -0800

    AutoPush

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,11 +1,7 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stddef.h>
-#include <stdbool.h>
-#include <unistd.h>
-#include <ctype.h>
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
+
+#include <stdlib.h>
 
 int _putchar(char);
 void print_name(char *name, void (*f)(char *));
