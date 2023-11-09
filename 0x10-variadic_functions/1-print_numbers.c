3fcdfff7976907be6b14e05c97b257b63b347937
ahmedtiko <ahmedmmtarek@gmail.com>
2023-11-09 07:22:01 -0800

    AutoPush

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -19,9 +19,9 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		printf("%d", va_arg(args, int));
 
-		if (seperator != NULL && i < n - 1)
+		if (separator != NULL && i < n - 1)
 		{
-			printf("%s", seperator);
+			printf("%s", separator);
 		}
 	}
 
