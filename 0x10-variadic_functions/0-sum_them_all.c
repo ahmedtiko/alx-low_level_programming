0f4a76f94369bcfe8e725425044d38f60b168bf1
ahmedtiko <ahmedmmtarek@gmail.com>
2023-11-09 07:03:46 -0800

    AutoPush

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,14 +9,14 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
+	va_list args;
+	unsigned int i;
+	int sum = 0;
+
 	if (n == 0)
 	{
 		return (0);
 	}
-
-	va_list args;
-	unsigned int i;
-	int sum = 0;
 
 	va_start(args, n);
 
