7e07e3e522d0f13fbe09a1f071bc0485846e9c56
ahmedtiko <ahmedmmtarek@gmail.com>
2023-11-09 08:38:23 -0800

    AutoPush

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -9,6 +9,21 @@
 #include <unistd.h>
 #include <ctype.h>
 
+/**
+ * struct print_function - Struct for mapping format specifiers to functions.
+ * @specifier: The format specifier.
+ * @printer: The corresponding printer function.
+ */
+
+typedef struct print_function
+{
+	char specifier;
+	void (*printer)(va_list);
+} print_function_t;
+void print_char(va_list ap);
+void print_int(va_list ap);
+void print_float(va_list ap);
+void print_string(va_list ap);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
