fbb205b44de67ecd4cda847268b834730eddb77f
ahmedtiko <ahmedmmtarek@gmail.com>
2023-11-16 18:47:01 -0800

    AutoPush

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+
+
+/**
+ * print_before_main - Prints specific lines before main function is executed.
+ */
+void __attribute__((constructor)) print_before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
