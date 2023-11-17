ff54be730ad9f2fc6971f77ede6f0aa11cf9f511
ahmedtiko <ahmedmmtarek@gmail.com>
2023-11-16 18:15:01 -0800

    AutoPush

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,66 @@
+#include "lists.h"
+
+/**
+ * _strlen - Computes the length of a string.
+ * @str: The string to be measured.
+ * Return: The length of the string.
+ */
+size_t _strlen(const char *str)
+{
+	size_t len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	return (len);
+}
+
+/**
+ * add_node_end - Adds a new node at the end of a list_t list.
+ * @head: Pointer to a pointer to the head of the list.
+ * @str: String to be duplicated and added as the content of the new node.
+ * Return: The address of the new element, or NULL if it failed.
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new_node, *last_node;
+
+	if (head == NULL || str == NULL)
+	{
+		return (NULL);
+	}
+
+	new_node = malloc(sizeof(list_t));
+	if(new_node == NULL)
+	{
+		return (NULL);
+	}
+
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->len = _strlen(new_node->str);
+	new_node->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new_node)
+	}
+	else
+	{
+		last_node = *head;
+		while (last_node->next != NULL)
+		{
+			last_node = last_node->next;
+			last_node->next = new_node;
+		}
+	}
+
+	return (new_node);
+}
