196ea12813d7eddbc4d06637343bd6e58725b308
ahmedtiko <ahmedmmtarek@gmail.com>
2023-11-16 19:02:00 -0800

    AutoPush

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -53,8 +53,8 @@ list_t *add_node_end(list_t **head, const char *str)
 		while (last_node->next != NULL)
 		{
 			last_node = last_node->next;
-			last_node->next = new_node;
 		}
+		last_node->next = new_node;
 	}
 
 	return (new_node);
