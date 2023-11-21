466a1a0a77cdf0fbaa7fbcf21acc75f8b3091948
ahmedtiko <ahmedmmtarek@gmail.com>
2023-11-21 10:30:51 -0800

    AutoPush

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -42,7 +42,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (idx == 0)
 	{
-		return add_nodeint(head, n);
+		return (add_nodeint(head, n));
 	}
 
 	current = *head;
