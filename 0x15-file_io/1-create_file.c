2e1ab1a04e4c5d84547acdfbce54d69796342c5a
ahmedtiko <ahmedmmtarek@gmail.com>
2023-11-27 14:08:21 -0800

    AutoPush

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * create_file - Creates a file with specified permissions & writes text to it
+ * @filename: The name of the file to create.
+ * @text_content: A NULL terminated string to write to the file.
+ *
+ * Return: 1 on success, -1 on failure.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int file_d, write_s, i;
+
+	if (file_name == NULL)
+	{
+		return (0);
+	}
+
+	file_d = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (file_d == -1)
+	{
+		return (-1);
+	}
+
+	if (text_content != NULL)
+	{
+		for (i = 0; text_content[i] != '\0'; i++)
+			;
+
+		write_s = write(file_d, text_content, i);
+		if (write_s == -1)
+		{
+			close(file_d);
+			return (-1);
+		}
+	}
+	close(file_d);
+	return (1);
+}
