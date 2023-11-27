9508eb8bdddb58ec40b82f4772b4c7194b03fa53
ahmedtiko <ahmedmmtarek@gmail.com>
2023-11-27 13:28:35 -0800

    AutoPush

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <string.h>
+
+int _putchar(char);
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+
+#endif
