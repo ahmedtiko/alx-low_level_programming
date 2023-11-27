a9494b208390e369165674a7a24a9b74bc14a142
ahmedtiko <ahmedmmtarek@gmail.com>
2023-11-27 15:57:57 -0800

    AutoPush

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,73 @@
+#include "main.h"
+
+/**
+ * display_error - Display error message and exit with status code 98.
+ * @message: The error message to display.
+ */
+void display_error(const char *message)
+{
+	fprintf(stderr, "%s\n", message);
+	exit(98);
+}
+
+/**
+ * display_elf_header - Display information from the ELF header.
+ * @header: Pointer to the ELF header structure.
+ */
+void display_elf_header(const Elf64_Ehdr *header)
+{
+	printf("ELF Header:\n");
+	printf("  Magic:   ");
+	for (int i = 0; i < EI_NIDENT; ++i)
+	{
+		printf("%02x ", header->e_ident[i]);
+	}
+	printf("\n");
+	printf("  Class:                             %s\n", header->e_ident[EI_CLASS] == ELFCLASS64 ? "ELF64" : "ELF32");
+	printf("  Data:                              %s\n", header->e_ident[EI_DATA] == ELFDATA2MSB ? "2's complement, big endian" : "2's complement, little endian");
+	printf("  Version:                           %d (current)\n", header->e_ident[EI_VERSION]);
+	printf("  OS/ABI:                            %s\n", header->e_ident[EI_OSABI] == ELFOSABI_SYSV ? "UNIX - System V" : "Other");
+	printf("  ABI Version:                       %d\n", header->e_ident[EI_ABIVERSION]);
+	printf("  Type:                              %s\n", header->e_type == ET_EXEC ? "EXEC (Executable file)" : "Other");
+	printf("  Entry point address:               %#lx\n", (unsigned long)header->e_entry);
+}
+
+/**
+ * main - Entry point for the program.
+ * @argc: The number of command-line arguments.
+ * @argv: An array containing the command-line arguments.
+ * Return: 0 on success, 98 on failure.
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		fprintf(stderr, "Usage: %s elf_filename\n", argv[0]);
+		exit(98);
+	}
+
+	int fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+	{
+		display_error("Error: Unable to open the file");
+	}
+
+	Elf64_Ehdr elf_header;
+	if (read(fd, &elf_header, sizeof(Elf64_Ehdr)) != sizeof(Elf64_Ehdr))
+	{
+		display_error("Error: Unable to read ELF header");
+	}
+	if (elf_header.e_ident[EI_MAG0] != ELFMAG0 || elf_header.e_ident[EI_MAG1] != ELFMAG1 ||
+			elf_header.e_ident[EI_MAG2] != ELFMAG2 || elf_header.e_ident[EI_MAG3] != ELFMAG3)
+	 {
+		 display_error("Error: Not an ELF file");
+	 }
+	 display_elf_header(&elf_header);
+
+	 if (close(fd) == -1)
+	 {
+		 display_error("Error: Unable to close the file");
+	 }
+
+	 return (0);
+}
