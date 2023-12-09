d37defea8144290125d9c1048934003dbb3ec28c
ahmedtiko <ahmedmmtarek@gmail.com>
2023-12-09 11:09:06 -0800

    AutoPush

--- test/lists.c
+++ test/lists.c
@@ -104,7 +104,7 @@ size_t print_list_str(const list_t *h)
 int delete_node_at_index(list_t **head, unsigned int index)
 {
 	list_t *node, *prev_node;
-	unsigned int i = 0;
+	unsigned int x = 0;
 
 	if (!head || !*head)
 		return (0);
@@ -120,14 +120,14 @@ int delete_node_at_index(list_t **head, unsigned int index)
 	node = *head;
 	while (node)
 	{
-		if (i == index)
+		if (x == index)
 		{
 			prev_node->next = node->next;
 			free(node->str);
 			free(node);
 			return (1);
 		}
-		i++;
+		x++;
 		prev_node = node;
 		node = node->next;
 	}
