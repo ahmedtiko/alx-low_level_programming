d15b82a511960553f65d1063695c94e126d7497d
GeorgeGhabour22 <95974387+GeorgeGhabour22@users.noreply.github.com>
2023-12-09 21:07:23 +0200

    Update string.c

--- test/string.c
+++ test/string.c
@@ -1,10 +1,10 @@
 #include "shell.h"
 
 /**
- * _strlen - returns length of string
- * @s: the string whose length to check
+ * _strlen - Return the length of the string.
+ * @s: the string whose length to check.
  *
- * Return: integer length of string
+ * Return: integer length of string.
  */
 int _strlen(char *s)
 {
@@ -19,9 +19,9 @@ int _strlen(char *s)
 }
 
 /**
- * _strcmp - performs comparison of two strangs.
- * @s1: the first strang
- * @s2: the second strang
+ * _strcmp - performs the comparison of two strings.
+ * @s1: first string.
+ * @s2: second string.
  *
  * Return: -ve if s1 < s2, +ve if s1 > s2, 0 if s1 == s2
  */
@@ -41,11 +41,11 @@ int _strcmp(char *s1, char *s2)
 }
 
 /**
- * starts_with - to check if needle starts with haystack
- * @haystack: string to search
- * @needle: the substring to find
+ * starts_with - To check if the needle starts with haystack.
+ * @haystack: String to search.
+ * @needle: Substring to find.
  *
- * Return: address of next char of haystack or NULL
+ * Return: Address of next char of haystack or NULL.
  */
 char *starts_with(const char *haystack, const char *needle)
 {
@@ -56,11 +56,11 @@ char *starts_with(const char *haystack, const char *needle)
 }
 
 /**
- * _strcat - concatenates two strings
- * @dest: the destination buffer
- * @src: the source buffer
+ * _strcat - Concatenate two strings.
+ * @dest: Destination buffer.
+ * @src: the buffer source.
  *
- * Return: pointer to destination buffer
+ * Return: Pointer to the destination buffer.
  */
 char *_strcat(char *dest, char *src)
 {
