7cc664af077d73225d0ee552811c454fc4ed8290
ahmedtiko <ahmedmmtarek@gmail.com>
2023-12-09 09:22:00 -0800

    AutoPush

--- test/string1.c
+++ test/string1.c
@@ -9,16 +9,16 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int i = 0;
+	int index = 0;
 
 	if (dest == src || src == 0)
 		return (dest);
-	while (src[i])
+	while (src[index])
 	{
-		dest[i] = src[i];
-		i++;
+		dest[index] = src[index];
+		index++;
 	}
-	dest[i] = 0;
+	dest[index] = 0;
 	return (dest);
 }
 
@@ -30,19 +30,19 @@ char *_strcpy(char *dest, char *src)
  */
 char *_strdup(const char *str)
 {
-	int length = 0;
-	char *ret;
+	int l = 0;
+	char *r;
 
 	if (str == NULL)
 		return (NULL);
 	while (*str++)
-		length++;
-	ret = malloc(sizeof(char) * (length + 1));
-	if (!ret)
+		l++;
+	r = malloc(sizeof(char) * (l + 1));
+	if (!r)
 		return (NULL);
-	for (length++; length--;)
-		ret[length] = *--str;
-	return (ret);
+	for (l++; l--;)
+		ret[l] = *--str;
+	return (r);
 }
 
 /**
@@ -53,14 +53,14 @@ char *_strdup(const char *str)
  */
 void _puts(char *str)
 {
-	int i = 0;
+	int index = 0;
 
 	if (!str)
 		return;
-	while (str[i] != '\0')
+	while (str[index] != '\0')
 	{
-		_putchar(str[i]);
-		i++;
+		_putchar(str[index]);
+		index++;
 	}
 }
 
@@ -73,15 +73,15 @@ void _puts(char *str)
  */
 int _putchar(char c)
 {
-	static int i;
-	static char buf[WRITE_BUF_SIZE];
+	static int index;
+	static char buff[WRITE_BUF_SIZE];
 
-	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	if (c == BUF_FLUSH || index >= WRITE_BUF_SIZE)
 	{
-		write(1, buf, i);
-		i = 0;
+		write(1, buff, index);
+		index = 0;
 	}
 	if (c != BUF_FLUSH)
-		buf[i++] = c;
+		buff[index++] = c;
 	return (1);
 }
