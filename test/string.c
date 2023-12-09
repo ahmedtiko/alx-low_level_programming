e095bfafb64350ecf616907b7cb3f599e4a7dd83
ahmedtiko <ahmedmmtarek@gmail.com>
2023-12-09 09:09:24 -0800

    AutoPush

--- test/string.c
+++ test/string.c
@@ -1,29 +1,29 @@
 #include "shell.h"
 
 /**
- * _strlen - returns the length of a string
+ * _strlen - returns length of string
  * @s: the string whose length to check
  *
  * Return: integer length of string
  */
 int _strlen(char *s)
 {
-	int i = 0;
+	int index = 0;
 
 	if (!s)
 		return (0);
 
 	while (*s++)
-		i++;
-	return (i);
+		index++;
+	return (index);
 }
 
 /**
- * _strcmp - performs lexicogarphic comparison of two strangs.
+ * _strcmp - performs comparison of two strangs.
  * @s1: the first strang
  * @s2: the second strang
  *
- * Return: negative if s1 < s2, positive if s1 > s2, zero if s1 == s2
+ * Return: -ve if s1 < s2, +ve if s1 > s2, 0 if s1 == s2
  */
 int _strcmp(char *s1, char *s2)
 {
@@ -41,7 +41,7 @@ int _strcmp(char *s1, char *s2)
 }
 
 /**
- * starts_with - checks if needle starts with haystack
+ * starts_with - to check if needle starts with haystack
  * @haystack: string to search
  * @needle: the substring to find
  *
@@ -64,12 +64,12 @@ char *starts_with(const char *haystack, const char *needle)
  */
 char *_strcat(char *dest, char *src)
 {
-	char *ret = dest;
+	char *r = dest;
 
 	while (*dest)
 		dest++;
 	while (*src)
 		*dest++ = *src++;
 	*dest = *src;
-	return (ret);
+	return (r);
 }
