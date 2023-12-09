c807048e0036f45eb4bb9c68a015c1b43747627a
ahmedtiko <ahmedmmtarek@gmail.com>
2023-12-09 11:10:34 -0800

    AutoPush

--- test/main.c
+++ test/main.c
@@ -10,7 +10,7 @@
 int main(int ac, char **av)
 {
 	info_t info[] = { INFO_INIT };
-	int fd = 2;
+	int fds = 2;
 
 	asm ("mov %1, %0\n\t"
 		"add $3, %0"
@@ -19,8 +19,8 @@ int main(int ac, char **av)
 
 	if (ac == 2)
 	{
-		fd = open(av[1], O_RDONLY);
-		if (fd == -1)
+		fds = open(av[1], O_RDONLY);
+		if (fds == -1)
 		{
 			if (errno == EACCES)
 				exit(126);
@@ -35,7 +35,7 @@ int main(int ac, char **av)
 			}
 			return (EXIT_FAILURE);
 		}
-		info->readfd = fd;
+		info->readfd = fds;
 	}
 	populate_env_list(info);
 	read_history(info);
