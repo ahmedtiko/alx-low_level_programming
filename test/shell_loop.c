844865cee8ff309139fbe13f3403623a373cd84a
GeorgeGhabour22 <95974387+GeorgeGhabour22@users.noreply.github.com>
2023-12-09 21:01:58 +0200

    Update shell_loop.c

--- test/shell_loop.c
+++ test/shell_loop.c
@@ -1,11 +1,11 @@
 #include "shell.h"
 
 /**
- * hsh - main shell loop
- * @info: the parameter & return info struct
- * @av: the argument vector from main()
+ * hsh - Main shell loop.
+ * @info: The parameter & return info structure.
+ * @av: The args vector from main().
  *
- * Return: 0 on success, 1 on error, or error code
+ * Return: 0 on success, 1 on error, or error code.
  */
 int hsh(info_t *info, char **av)
 {
@@ -52,13 +52,13 @@ int hsh(info_t *info, char **av)
 }
 
 /**
- * find_builtin - finds a builtin command
- * @info: the parameter & return info struct
+ * find_builtin - Finds a builtin command.
+ * @info: The parameter & return info structure.
  *
  * Return: -1 if builtin not found,
  *			0 if builtin executed successfully,
  *			1 if builtin found but not successful,
- *			-2 if builtin signals exit()
+ *			-2 if builtin signals exit().
  */
 int find_builtin(info_t *info)
 {
@@ -86,10 +86,10 @@ int find_builtin(info_t *info)
 }
 
 /**
- * find_cmd - finds a command in PATH
- * @info: the parameter & return info struct
+ * find_cmd - Finds a command in the path.
+ * @info: the parameter & return info structure.
  *
- * Return: void
+ * Return: void.
  */
 void find_cmd(info_t *info)
 {
@@ -128,10 +128,10 @@ void find_cmd(info_t *info)
 }
 
 /**
- * fork_cmd - forks a an exec thread to run cmd
- * @info: the parameter & return info struct
+ * fork_cmd - Forks a, an executed thread to run cmd.
+ * @info: the parameter & return info struct.
  *
- * Return: void
+ * Return: void.
  */
 void fork_cmd(info_t *info)
 {
