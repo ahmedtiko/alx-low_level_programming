739a20cda78e70b87e01a413f189936a297e548b
GeorgeGhabour22 <95974387+GeorgeGhabour22@users.noreply.github.com>
2023-12-09 19:53:07 +0200

    Update getenv.c

--- test/getenv.c
+++ test/getenv.c
@@ -1,10 +1,10 @@
 #include "shell.h"
 
 /**
- * get_environ - returns the string array copy of our environ
+ * get_environ - Return the string array copied from our environment.
  * @info: Structure containing potential arguments. Used to maintain
  *          constant function prototype.
- * Return: Always 0
+ * Return: Always 0.
  */
 char **get_environ(info_t *info)
 {
@@ -18,11 +18,11 @@ char **get_environ(info_t *info)
 }
 
 /**
- * _unsetenv - Remove an environment variable
+ * _unsetenv - Remove the environment variable.
  * @info: Structure containing potential arguments. Used to maintain
  *        constant function prototype.
  *  Return: 1 on delete, 0 otherwise
- * @var: the string env var property
+ * @var: the string env var property.
  */
 int _unsetenv(info_t *info, char *var)
 {
@@ -50,13 +50,13 @@ int _unsetenv(info_t *info, char *var)
 }
 
 /**
- * _setenv - Initialize a new environment variable,
- *             or modify an existing one
- * @info: Structure containing potential arguments. Used to maintain
+ * _setenv - initialize a new environment variable,
+ *             or modify the existing one.
+ * @info: structure containing potential arguments. Used to maintain
  *        constant function prototype.
- * @var: the string env var property
- * @value: the string env var value
- *  Return: Always 0
+ * @var: String env var property.
+ * @value: String env var value.
+ *  Return: Always 0.
  */
 int _setenv(info_t *info, char *var, char *value)
 {
