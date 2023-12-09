06b83d9ededb63bfdcf611e7736c0743a9448713
GeorgeGhabour22 <95974387+GeorgeGhabour22@users.noreply.github.com>
2023-12-09 20:51:07 +0200

    Update realloc.c

--- test/realloc.c
+++ test/realloc.c
@@ -1,11 +1,11 @@
 #include "shell.h"
 
 /**
- **_memset - fills memory with a constant byte
- *@s: the pointer to the memory area
- *@b: the byte to fill *s with
- *@n: the amount of bytes to be filled
- *Return: (s) a pointer to the memory area s
+ **_memset - Fill the memory with a constant byte.
+ *@s: The pointer into the memory area.
+ *@b: The byte to fill *s with.
+ *@n: The number of bytes to be filled.
+ *Return: (s) A pointer to the memory area s.
  */
 char *_memset(char *s, char b, unsigned int n)
 {
@@ -17,8 +17,8 @@ char *_memset(char *s, char b, unsigned int n)
 }
 
 /**
- * ffree - frees a string of strings
- * @pp: string of strings
+ * ffree - free the string of strings.
+ * @pp: string of strings.
  */
 void ffree(char **pp)
 {
@@ -32,9 +32,9 @@ void ffree(char **pp)
 }
 
 /**
- * _realloc - reallocates a block of memory
- * @ptr: pointer to previous block that was malloced
- * @old_size: size of old block
+ * _realloc - Reallocating a block of memory.
+ * @ptr: Pointer to the previous block that was malloced.
+ * @old_size: the size of the old block
  * @new_size: size of new block
  *
  * Return: pointer to reallocated memory block.
