9dd22cbb72d3341c17751c16065b14d18aec592c
ahmedtiko <33915834+ahmedtiko@users.noreply.github.com>
2023-12-10 14:39:27 +0200

    Update _atoi.c

--- test/_atoi.c
+++ test/_atoi.c
@@ -1,7 +1,7 @@
 #include "shell.h"
 
 /**
- * interact - returns true if shell is in the interactive mode.
+ * interactive - returns true if shell is in the interactive mode.
  * @info: struct address.
  *
  * Return: 1 if interactive mode, 0 otherwise.
