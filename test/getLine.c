7ad352bfe83f4a4ccff1046dce38e9b6a4375b46
ahmedtiko <33915834+ahmedtiko@users.noreply.github.com>
2023-12-12 20:24:29 +0200

    Update getLine.c

--- test/getLine.c
+++ test/getLine.c
@@ -31,7 +31,7 @@ ssize_t input_buf(info_t *info, char **buf, size_t *len)
 				r--;
 			}
 			info->linecount_flag = 1;
-			remove_comments(*buf);
+			rm_comm(*buf);
 			build_history_list(info, *buf, info->histcount++);
 			/* if (_strchr(*buf, ';')) is this a command chain? */
 			{
