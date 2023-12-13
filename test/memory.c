c68fa29dee5ecdf94a1c8673149c1ddba77dab0f
ahmedtiko <33915834+ahmedtiko@users.noreply.github.com>
2023-12-13 19:23:21 +0200

    Update memory.c

--- test/memory.c
+++ test/memory.c
@@ -1,12 +1,12 @@
 #include "simple.h"
 
 /**
- * bfree - free the pointer and NULLS the addresses.
+ * be_free - free the pointer and NULLS the addresses.
  * @ptr: address of the pointer to free.
  *
  * Return: 1 if freed, otherwise 0.
  */
-int bfree(void **ptr)
+int be_free(void **ptr)
 {
 	if (ptr && *ptr)
 	{
