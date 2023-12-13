f914465550dffcf56f1f6fa621467c07d205f9b4
ahmedtiko <33915834+ahmedtiko@users.noreply.github.com>
2023-12-13 22:03:20 +0200

    Update memory.c

--- test/memory.c
+++ test/memory.c
@@ -1,10 +1,16 @@
 #include "simple.h"
 
 /**
- * be_free - free the pointer and NULLS the addresses.
- * @ptr: address of the pointer to free.
+ * be_free - Frees the allocated memory and sets the pointer to NULL.
+ * @ptr: A pointer to the memory to be freed.
  *
- * Return: 1 if freed, otherwise 0.
+ * Description:
+ *	Checks if the pointer is not NULL and frees the allocated memory.
+ *	After freeing the memory, it sets the pointer to NULL to avoid
+ *	dangling pointers.
+ *
+ * Return:
+ *	Returns 1 on successful freeing of memory, 0 otherwise.
  */
 int be_free(void **ptr)
 {
