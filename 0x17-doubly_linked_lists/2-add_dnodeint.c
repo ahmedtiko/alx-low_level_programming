2b513d380f60cacfed0582bdb4fd53f6865ca0b9
ahmedtiko <ahmedmmtarek@gmail.com>
2024-01-11 09:28:41 -0800

    Auto Push

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -14,6 +14,10 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (head == NULL)
 		return (NULL);
 
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+
 	new_node->n = n;
 	new_node->prev = NULL;
 	new_node->next = *head;
